4a49d37c1f6c8483f63aeec003b942dcc9b8e88f
Mykola Kuzevych <m.kuzevych@gmail.com>
2020-07-11 14:30:39 +0300

    Updates

---
+++ precache-manifest.2b6d9801f9fce3f4aa6e2b06940aef8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb98863b2d21d3daa8acb9003e2ceeaa",
+    "url": "/azsoft/index.html"
+  },
+  {
+    "revision": "392a213510a00a8942aa",
+    "url": "/azsoft/static/css/main.12052213.chunk.css"
+  },
+  {
+    "revision": "380fd9cb8c9c7f67ed6d",
+    "url": "/azsoft/static/js/2.fcebd4e2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/azsoft/static/js/2.fcebd4e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "392a213510a00a8942aa",
+    "url": "/azsoft/static/js/main.1860295d.chunk.js"
+  },
+  {
+    "revision": "1c4ea83005db0b96e515",
+    "url": "/azsoft/static/js/runtime-main.f238f554.js"
+  }
+]);
